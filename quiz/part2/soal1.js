762018db9230cfe9fb22798b0d9583bf0030418c
jmmarr2025 <jm.marr2025@gmail.com>
2025-02-27 14:22:50 +0700

    push soal 1 sampai 4

---
+++ quiz/part2/soal1.js
@@ -0,0 +1,13 @@
+// SOAL 1
+
+let word = 'JavaScript';
+let second = 'is';
+let third = 'awesome';
+let fourth = 'and';
+let fifth = 'I';
+let sixth = 'love';
+let seventh = 'it!';
+
+let kalimatLengkap = word + " " + second + " " + third + " " + fourth + " " + fifth + " " + sixth + " " + seventh;
+console.log(kalimatLengkap);
+
